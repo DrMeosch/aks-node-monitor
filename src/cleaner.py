abd0a2d9b8b6959f59a9602593e8457608a510c7
DrMeosch <51243387+DrMeosch@users.noreply.github.com>
2024-04-06 13:16:29 +0200

    add pv cleaner

---
+++ src/cleaner.py
@@ -0,0 +1,47 @@
+from kubernetes import client, config
+from kubernetes.client.rest import ApiException
+
+import time
+from kubernetes import client
+
+# https://github.com/kubernetes-client/python/blob/master/examples/in_cluster_config.py
+config.load_incluster_config()
+
+
+def main():
+    while True:
+        v1 = client.CoreV1Api()
+        nodes = [node.metadata.name for node in v1.list_node(watch=False)]
+        try:
+            pvs = v1.list_persistent_volume(watch=False)
+        except ApiException as e:
+            print(f"Error: {e}")
+        # deleting PVs on nodes that are not in the cluster
+        for pv in pvs.items:
+            if not pv.metadata.name.startswith("local-pv-"):
+                continue
+            if (
+                pv.spec.node_affinity
+                and pv.spec.node_affinity.required.node_selector_terms
+            ):
+                for term in pv.spec.node_affinity.required.node_selector_terms:
+                    for expr in term.match_expressions:
+                        if (
+                            expr.key == "kubernetes.io/hostname"
+                            # assume only one value in the list
+                            and expr.values[0] not in nodes
+                        ):
+                            try:
+                                v1.delete_persistent_volume(pv.metadata.name)
+                                print(
+                                    "PVs of node successfully deleted: {}".format(
+                                        pv.metadata.name
+                                    )
+                                )
+                            except ApiException as e:
+                                print(f"Error: {e}")
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    main()
